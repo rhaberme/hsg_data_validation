d747122254a20f6abf21413c7fea7fe2aa3c2f62
rhaberme <rhaberme@rhrk.uni-kl.de>
2025-03-21 16:16:23 +0000

    Changed test_plau.py to match the new column names for validated data

--- tests/test_plau.py
+++ tests/test_plau.py
@@ -13,6 +13,6 @@ def test_jumps():
     df_data = pd.read_csv("tests/test_data/data_shift.csv")
     value_col_name = "Temperature [°C]"
     isjump = check_for_jumps(df_data, window=2, threshold=1, value_col_name=value_col_name)
-    true_correct = all(isjump["Jump"].iloc[[*range(39, 43), *range(58, 62), *range(104, 108)]])
-    false_correct = not any(isjump["Jump"].iloc[[*range(0, 39), *range(43, 58), *range(62, 104), *range(108, 126)]])
+    true_correct = all(isjump["Error"].iloc[[*range(39, 43), *range(58, 62), *range(104, 108)]])
+    false_correct = not any(isjump["Error"].iloc[[*range(0, 39), *range(43, 58), *range(62, 104), *range(108, 126)]])
     assert true_correct and false_correct
